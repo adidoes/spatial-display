077a1d3f189e75b845956bd36a7839e7b454174e
Adrian <git@adrian.email>
2024-10-30 01:27:56 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,21 @@ use ar_drivers::lib::{any_glasses, GlassesEvent};
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::diagnostic::LogDiagnosticsPlugin;
 use bevy::prelude::*;
+use bevy::render::render_asset::RenderAssetUsages;
 use bevy::render::render_resource::Extent3d;
 use bevy::render::render_resource::TextureDimension;
-use bevy::render::{
-    mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology,
-};
 use bevy::window::{
-    Monitor, MonitorSelection, PresentMode, Window, WindowLevel, WindowMode, WindowPlugin,
-    WindowPosition,
-};
-
-use bevy::{
-    render::camera::RenderTarget,
-    window::{ExitCondition, WindowRef},
+    MonitorSelection, PresentMode, Window, WindowLevel, WindowMode, WindowPlugin, WindowPosition,
 };
 
 use bevy::render::render_resource::{TextureFormat, TextureUsages};
 use core_foundation::base::TCFType;
 use core_media::sample_buffer::{CMSampleBuffer, CMSampleBufferRef};
 use core_video::pixel_buffer::kCVPixelBufferLock_ReadOnly;
-use core_video::pixel_buffer::kCVPixelFormatType_32BGRA;
 use core_video::pixel_buffer::CVPixelBuffer;
-use core_video::pixel_buffer::CVPixelBufferLockFlags;
 use dispatch2::{Queue, QueueAttribute};
 use objc2::{
-    declare_class, extern_methods, msg_send_id, mutability,
+    declare_class, msg_send_id, mutability,
     rc::{Allocated, Id},
     runtime::ProtocolObject,
     ClassType, DeclaredClass,
@@ -45,13 +35,12 @@ use screen_capture_kit::stream::SCStreamOutput;
 use screen_capture_kit::stream::SCStreamOutputType;
 use screen_capture_kit::{
     shareable_content::SCShareableContent,
-    stream::{SCFrameStatus, SCStream, SCStreamConfiguration},
+    stream::{SCStream, SCStreamConfiguration},
 };
 
-use std::sync::{Arc, Mutex};
+use dcmimu::DCMIMU;
 
-#[derive(Component)]
-struct MonitorRef(Entity);
+use std::sync::{Arc, Mutex};
 
 // Shared state between capture thread and bevy
 #[derive(Debug)]
@@ -122,8 +111,6 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Startup, create_glasses_thread)
         .add_systems(FixedUpdate, update_texture)
-        // .add_systems(Update, handle_keyboard_input)
-        // .add_systems(Update, (update, close_on_esc))
         // You can do First/PreUpdate/Update or FixedFirst/FixedPreUpdate/FixedUpdate
         .add_systems(FixedPreUpdate, glasses_event_system)
         .insert_resource(Time::<Fixed>::from_hz(500.0)) // when using Fixed schedule
@@ -135,91 +122,6 @@ fn main() {
         .run();
 }
 
-fn handle_keyboard_input(
-    mut commands: Commands,
-    focused_windows: Query<(Entity, &Window)>,
-    input: Res<ButtonInput<KeyCode>>,
-) {
-    for (window, focus) in focused_windows.iter() {
-        if (!focus.focused) {
-            continue;
-        }
-
-        if (input.just_pressed(KeyCode::Escape)) {
-            commands.entity(window).despawn();
-        }
-    }
-}
-
-fn update(
-    mut commands: Commands,
-    monitors_added: Query<(Entity, &Monitor), Added<Monitor>>,
-    mut monitors_removed: RemovedComponents<Monitor>,
-    monitor_refs: Query<(Entity, &MonitorRef)>,
-) {
-    for (entity, monitor) in monitors_added.iter() {
-        // Spawn a new window on each monitor
-        let name = monitor.name.clone().unwrap_or_else(|| "<no name>".into());
-        let size = format!("{}x{}px", monitor.physical_height, monitor.physical_width);
-        let hz = monitor
-            .refresh_rate_millihertz
-            .map(|x| format!("{}Hz", x as f32 / 1000.0))
-            .unwrap_or_else(|| "<unknown>".into());
-        let position = format!(
-            "x={} y={}",
-            monitor.physical_position.x, monitor.physical_position.y
-        );
-        let scale = format!("{:.2}", monitor.scale_factor);
-
-        let window = commands
-            .spawn((
-                Window {
-                    title: name.clone(),
-                    mode: WindowMode::Fullscreen(MonitorSelection::Entity(entity)),
-                    position: WindowPosition::Centered(MonitorSelection::Entity(entity)),
-                    ..default()
-                },
-                MonitorRef(entity),
-            ))
-            .id();
-
-        let camera = commands
-            .spawn((
-                Camera2d,
-                Camera {
-                    target: RenderTarget::Window(WindowRef::Entity(window)),
-                    ..default()
-                },
-            ))
-            .id();
-
-        let info_text = format!(
-            "Monitor: {name}\nSize: {size}\nRefresh rate: {hz}\nPosition: {position}\nScale: {scale}\n\n",
-        );
-        commands.spawn((
-            Text(info_text),
-            Node {
-                position_type: PositionType::Relative,
-                height: Val::Percent(100.0),
-                width: Val::Percent(100.0),
-                ..default()
-            },
-            TargetCamera(camera),
-            MonitorRef(entity),
-        ));
-    }
-
-    // Remove windows for removed monitors
-    for monitor_entity in monitors_removed.read() {
-        for (ref_entity, monitor_ref) in monitor_refs.iter() {
-            if (monitor_ref.0 == monitor_entity) {
-                commands.entity(ref_entity).despawn_recursive();
-            }
-        }
-    }
-}
-
-use dcmimu::DCMIMU;
 struct SharedGlassesStore {
     dcmimu: Arc<Mutex<DCMIMU>>,
 }
@@ -268,7 +170,7 @@ fn create_glasses_thread(shared_glasses_store: Res<SharedGlassesStore>) {
 
             loop_counter += 1;
 
-            if (last_print_time.elapsed() > Duration::from_secs(1)) {
+            if last_print_time.elapsed() > Duration::from_secs(1) {
                 println!("Loop has run {} times in the last second", loop_counter);
                 loop_counter = 0;
                 last_print_time = Instant::now();
@@ -351,16 +253,6 @@ fn setup(
         Transform::from_xyz(0.0, -4.0, 0.0),
     ));
 
-    // Test cube
-    commands.spawn((
-        Mesh3d(meshes.add(create_cube_mesh())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::linear_rgb(34.0, 43.0, 87.0),
-            ..default()
-        })),
-        Transform::from_xyz(2.0, 0.0, -5.0),
-    ));
-
     // Test spheres in different positions
     commands.spawn((
         Mesh3d(meshes.add(Sphere::default().mesh())),
@@ -393,43 +285,6 @@ fn setup(
     ));
 }
 
-fn create_screen_plane() -> Mesh {
-    let mut mesh = Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::RENDER_WORLD,
-    );
-
-    // Vertices of a square plane
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vec![
-            [-0.5, -0.5, 0.0], // bottom left
-            [0.5, -0.5, 0.0],  // bottom right
-            [0.5, 0.5, 0.0],   // top right
-            [-0.5, 0.5, 0.0],  // top left
-        ],
-    );
-
-    // UV coordinates for proper texture mapping
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            [0.0, 1.0], // bottom left
-            [1.0, 1.0], // bottom right
-            [1.0, 0.0], // top right
-            [0.0, 0.0], // top left
-        ],
-    );
-
-    // Normal vectors for lighting
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0.0, 0.0, 1.0]; 4]);
-
-    // Triangle indices
-    mesh.insert_indices(Indices::U32(vec![0, 2, 1, 0, 3, 2]));
-
-    mesh
-}
-
 fn update_texture(
     frame_data: Res<FrameDataResource>,
     screen_texture: Res<ScreenTexture>,
@@ -437,13 +292,13 @@ fn update_texture(
 ) {
     let mut shared = frame_data.shared_data.lock().unwrap();
 
-    if (!shared.new_frame) {
+    if !shared.new_frame {
         return;
     }
 
     if let Some(texture) = images.get_mut(&screen_texture.handle) {
         // Check if texture dimensions need updating
-        if (texture.width() != shared.width || texture.height() != shared.height) {
+        if texture.width() != shared.width || texture.height() != shared.height {
             *texture = Image::new(
                 Extent3d {
                     width: shared.width,
@@ -496,7 +351,7 @@ declare_class!(
             sample_buffer: CMSampleBufferRef,
             of_type: SCStreamOutputType,
         ) {
-            if (of_type != SCStreamOutputType::Screen) {
+            if of_type != SCStreamOutputType::Screen {
                 return;
             }
 
@@ -512,7 +367,7 @@ declare_class!(
                     pixel_buffer.lock_base_address(kCVPixelBufferLock_ReadOnly);
                     let data = unsafe { pixel_buffer.get_base_address() };
 
-                    if (data.is_null()) {
+                    if data.is_null() {
                         println!("❌ Pixel buffer base address is null");
                         return;
                     }
@@ -552,7 +407,7 @@ declare_class!(
                     // println!("   Match?: {}", rgba.len() == (width * height * 4) as usize);
 
                     // Update shared data only if we successfully created the buffer
-                    if (rgba.len() == (width * height * 4) as usize) {
+                    if rgba.len() == (width * height * 4) as usize {
                         if let Ok(mut shared) = self.ivars().shared_data.lock() {
                             // println!("💾 Updated shared buffer with {}x{} frame", width, height);
                             shared.width = width;
@@ -667,123 +522,4 @@ fn capture_screen(shared_data: Arc<Mutex<SharedFrameData>>) {
             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         }
     });
-}
-
-#[rustfmt::skip]
-fn create_cube_mesh() -> Mesh {
-    // Keep the mesh data accessible in future frames to be able to mutate it in toggle_texture.
-    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD)
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        // Each array is an [x, y, z] coordinate in local space.
-        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
-        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-        vec![
-            // top (facing towards +y)
-            [-0.5, 0.5, -0.5], // vertex with index 0
-            [0.5, 0.5, -0.5], // vertex with index 1
-            [0.5, 0.5, 0.5], // etc. until 23
-            [-0.5, 0.5, 0.5],
-            // bottom   (-y)
-            [-0.5, -0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            // right    (+x)
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-            [0.5, 0.5, -0.5],
-            // left     (-x)
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, 0.5, -0.5],
-            // back     (+z)
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            // forward  (-z)
-            [-0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [0.5, -0.5, -0.5],
-        ],
-    )
-    // Set-up UV coordinates to point to the upper (V < 0.5), "dirt+grass" part of the texture.
-    // Take a look at the custom image (assets/textures/array_texture.png)
-    // so the UV coords will make more sense
-    // Note: (0.0, 0.0) = Top-Left in UV mapping, (1.0, 1.0) = Bottom-Right in UV mapping
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            // Assigning the UV coords for the top side.
-            [0.0, 0.2], [0.0, 0.0], [1.0, 0.0], [1.0, 0.25],
-            // Assigning the UV coords for the bottom side.
-            [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
-            // Assigning the UV coords for the right side.
-            [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-            // Assigning the UV coords for the left side.
-            [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-            // Assigning the UV coords for the back side.
-            [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-            // Assigning the UV coords for the forward side.
-            [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-        ],
-    )
-    // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
-    // the surface.
-    // Normals are required for correct lighting calculations.
-    // Each array represents a normalized vector, which length should be equal to 1.0.
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![
-            // Normals for the top side (towards +y)
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            // Normals for the bottom side (towards -y)
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            // Normals for the right side (towards +x)
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            // Normals for the left side (towards -x)
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            // Normals for the back side (towards +z)
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            // Normals for the forward side (towards -z)
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-        ],
-    )
-    // Create the triangles out of the 24 vertices we created.
-    // To construct a square, we need 2 triangles, therefore 12 triangles in total.
-    // To construct a triangle, we need the indices of its 3 defined vertices, adding them one
-    // by one, in a counter-clockwise order (relative to the position of the viewer, the order
-    // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
-    // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
-    // further examples and the implementation of the built-in shapes.
-    .with_inserted_indices(Indices::U32(vec![
-        0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
-        4,5,7 , 5,6,7, // bottom (-y)
-        8,11,9 , 9,11,10, // right (+x)
-        12,13,15 , 13,14,15, // left (-x)
-        16,19,17 , 17,19,18, // back (+z)
-        20,21,23 , 21,22,23, // forward (-z)
-    ]))
 }
